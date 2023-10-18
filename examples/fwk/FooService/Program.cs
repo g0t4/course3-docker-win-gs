4b29f3c2a1a9b36b76ea1b15fd6d195882f47616
Wes Higbee <wes.mcclure@gmail.com>
2023-10-18 17:48:41 -0500

    copy over foo service example

---
+++ examples/fwk/FooService/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace FooService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("environment:");
+            Console.WriteLine($"  OS: {RuntimeInformation.OSDescription}");
+            Console.WriteLine($"  Fwk: {RuntimeInformation.FrameworkDescription}");
+            Console.WriteLine();
+
+            Console.WriteLine("initializing Foo service...");
+            Thread.Sleep(1000);
+            
+            Console.WriteLine("running...");
+            Thread.Sleep(3000);
+        }
+    }
+}
